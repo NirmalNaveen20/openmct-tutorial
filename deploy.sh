2e923a98ed3f638be6c65c7e576c0c99f451cb82
Nirmal Naveen <nirmalnaveen95@gmail.com>
2023-06-26 16:22:23 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-hostname=$(curl http://169.254.169.254/metadata/v1/hostname)
+hostname=$(curl http://159.223.105.181/metadata/v1/hostname)
 docker run -d -p 80:8080 --name openmct-"$hostname" NirmalNaveen20/openmct:"$hostname"
